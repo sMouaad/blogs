d61d0c6401031307059babaa0ff761eaf9b6b23c
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-09-24 19:00:42 +0100

    feat: add security by authenticating user in CREATE UPDATE DELETE operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "Fouaad", password: "FEDFADA", except: [ :index, :show ]
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "Fouaad", password: "FEDFADA", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
